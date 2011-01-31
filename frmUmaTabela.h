fde938959cf07cc6e6d2a9ed94708080827ee6d8
amg1127 <amg1127@5465fe1f-e9f4-4a51-a9b0-42098c041c11>
2011-01-31 12:23:01 +0000

    Submetendo alteracoes locais de outro computador...

--- frmUmaTabela.h
+++ frmUmaTabela.h
@@ -21,6 +21,21 @@ namespace Ui {
     class frmUmaTabela;
 }
 
+/*
+    Infelizmente, terei que reimplementar uma estrutura de tabela, linhas, colunas e celulas dentro do frmUmaTabela::ui->frmDadosTabela.
+    O widget QTableView não serve para os meus objetivos, porque ele produz uma barra de rolagem interna quando 
+dados de tabelas grandes precisam ser exibidos e nao ha forma de mudar esse comportamento.
+    Dentro do frmUmaTabela::ui->frmDadosTabela, sera criado um QGridLayout de 2x2 que terá:
+        # (0, 1) Um QSplitter com M QLabels para os rótulos das colunas (nomes dos campos)
+        # (1, 0) Um QSplitter com N QLabels para os rótulos das linhas (sequência numérica)
+        # (1, 1) Um QGridLayout de (M+1)x(N+1), sendo que:
+            # De (0, 0) a (M-1, N-1), serao criados QLabels para apresentar os dados
+            # Na linha M, serão criados QSpacerItem orientados horizontalmente e com 0px de altura, que servirão para definir a largura das colunas
+            # Na coluna N, serão criados QSpacerItem orientados verticalmente e com 0px de largura, que servirão para definir a altura das linhas
+    Quando os filhos do QSplitter do rótulo das linhas forem redimensionados, as alturas deverão propagar-se aos QSpacerItems da coluna N.
+    Quando os filhos do QSplitter do rótulo das colunas forem redimensionados, as larguras deverão propagar-se aos QSpacerItems da linha M.
+*/
+
 class button_spacer;
 
 class frmUmaTabela : public QWidget
